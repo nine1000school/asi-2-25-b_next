77ec932c61cf65e89d4090e9b47221d2405ba26b
Avétis KAZARIAN <avetis.k@gmail.com>
2024-01-09 14:31:16 +0100

    feat: single resource endpoints

--- src/pages/api/todos/[todoId].js
+++ src/pages/api/todos/[todoId].js
@@ -1,6 +1,10 @@
-const handler = (req, res) => {
-  const todoId = req.query.todoId
-  const todo = {} // database.query(todoId)
+import { readDatabase } from "@/database/readDatabase"
+import { writeDatabase } from "@/database/writeDatabase"
+
+const handler = async (req, res) => {
+  const { todoId } = req.query
+  const db = await readDatabase()
+  const todo = db.todos[todoId]
 
   if (!todo) {
     res.status(404).send({ error: "not found" })
@@ -10,21 +14,41 @@ const handler = (req, res) => {
 
   // GET /todos/[todoId] -> read resource item
   if (req.method === "GET") {
-    //
     res.send(todo)
+
+    return
   }
 
   // PATCH /todos/[todoId] -> update resource item
   if (req.method === "PATCH") {
-    //
-    const newTodo = { ...todo }
+    const { description } = req.body
+    const newTodo = { ...todo, description }
+
+    await writeDatabase({
+      ...db,
+      todos: {
+        ...db.todos,
+        [todoId]: newTodo,
+      },
+    })
 
     res.send(newTodo)
+
+    return
   }
 
   // DELETE /todos/[todoId] -> delete resource item
   if (req.method === "DELETE") {
-    //
+    const {
+      // eslint-disable-next-line no-unused-vars
+      todos: { [todoId]: _, ...todos },
+    } = db
+
+    await writeDatabase({
+      ...db,
+      todos,
+    })
+
     res.send(todo)
   }
 }
